1dd33cc67ebd1cc55155c3bffc92571d18aa82bb
daisai42 <144093314+daisai42@users.noreply.github.com>
2025-04-19 18:20:10 -0400

    Add files via upload
    Updated to add classification

--- Proj_Init.m
+++ Proj_Init.m
@@ -2,20 +2,20 @@ utilpath = 'C:\ProgramData\MATLAB\SupportPackages\R2019b\toolbox\imaq\supportpac
 addpath(utilpath);
 
 % The Kinect for Windows Sensor shows up as two separate devices in IMAQHWINFO.
-hwInfo = imaqhwinfo('kinect')
+hwInfo = imaqhwinfo('kinect');
 hwInfo.DeviceInfo(1)
 hwInfo.DeviceInfo(2)
 
 % Create the VIDEOINPUT objects for the two Kinect streams
-colorVid = videoinput('kinect',1)
-depthVid = videoinput('kinect',2)
+colorVid = videoinput('kinect',1);
+depthVid = videoinput('kinect',2);
 
 % Select Video Source for the Kinect
 depthSrc = getselectedsource(depthVid)
 depthSrc.EnableBodyTracking = 'on';
 
 % Set the Video Framing Data
-FramesPerTrigger = 90
+FramesPerTrigger = 90;
 triggerconfig([colorVid depthVid],'manual');
 colorVid.FramesPerTrigger = FramesPerTrigger;
 depthVid.FramesPerTrigger = FramesPerTrigger; 
@@ -49,3 +49,23 @@ SkeletonConnectionMap = [ [4 3];  % Neck
 
 % Marker colors for up to 6 bodies.
 colors = ['b';'r';'g';'c';'y';'m'];
+
+%Feature Data Init
+featureNames = {'MAV' 'LEN' 'ZC' 'SSC'};
+num_channels = 24; % 8 channels in myo band
+num_features = length(featureNames);
+%num_emg_samples = size(emg,1);
+
+windowSize = FramesPerTrigger; % number of samples to compute features
+binSize = 10; % number of sample to advance across samples
+zc_thresh = 0.2;  % threshold vaue for zero crossing
+ssc_thresh = 0.2; % threshold vaue for slope sign changes
+Fs = 1000; % sample rate
+
+% create indices for start and end of sliding data windows
+%idxStart = 1:binSize:num_emg_samples-windowSize-1;
+%idxEnd = idxStart+windowSize-1;
+%num_feature_samples = length(idxStart);
+
+% preallocate feature matrix
+%features = zeros(num_channels, num_features, num_feature_samples);
