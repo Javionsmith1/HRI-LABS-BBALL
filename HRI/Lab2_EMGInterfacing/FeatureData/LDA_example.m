87e3ff6a6385a8cda87b5a240c21539ecc50f0e8
DorN-cmd <doreen.h.ngo@gmail.com>
2025-04-27 13:55:09 -0400

    example LDA for reference

---
+++ HRI/Lab2_EMGInterfacing/FeatureData/LDA_example.m
@@ -0,0 +1,30 @@
+% Assume you have already trained the model:
+Mdl = fitcdiscr(X, Y);
+
+% Get linear coefficients for binary classification
+coef = Mdl.Coeffs(1,2);
+w = coef.Linear;
+
+% Rank features by absolute coefficient value
+[~, featureRank] = sort(abs(w), 'descend');
+
+% If X is a table, get variable names; else generate default names
+if istable(X)
+    featureNames = X.Properties.VariableNames;
+else
+    featureNames = strcat("Feature", string(1:size(X,2)));
+end
+
+% Reorder names and coefficients for plotting
+rankedNames = featureNames(featureRank);
+rankedWeights = w(featureRank);
+
+% Plot bar chart
+figure;
+bar(abs(rankedWeights));
+xticks(1:length(rankedNames));
+xticklabels(rankedNames);
+xtickangle(45);
+ylabel('Absolute Coefficient');
+title('Feature Importance from LDA Coefficients');
+grid on;
